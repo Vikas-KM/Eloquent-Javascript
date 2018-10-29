99cb2f4ee059ed5c83e559e7e57076e5da851e55
Vikas KM <vikki.km@gmail.com>
2018-10-29 23:33:38 +0530

    added about arrays

---
+++ Chapter 04/Chap04.js
@@ -0,0 +1,25 @@
+/*
+Properties that contain functions are generally called 
+methods of the value they belong to.
+
+The push method adds values to the end of an array, 
+and the pop method does the opposite, 
+removing the last value in the array and returning it.
+*/
+
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+// → [1, 2, 3, 4, 5]
+console.log(sequence.pop());
+// → 5
+console.log(sequence);
+// → [1, 2, 3, 4]
+
+/*
+The push method adds values to the end of an array, 
+and the pop method does the opposite, 
+removing the last value in the array and returning it.
+*/
+
